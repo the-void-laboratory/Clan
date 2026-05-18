7191a1105452abdb39240d5fa5e7d3913060cc69
the-void-laboratory <thevoidslab16@gmail.com>
2026-05-18 09:34:34 +0100

    new jasher

---
+++ config.js
@@ -0,0 +1,23 @@
+export default {
+  botToken: "8774231022:AAHPMjM5HaI6Exe7tySGV4u9pR1TsZP53rE", 
+
+  // API token
+  githubToken: "ghp_xxxxxxxxx", 
+  githubUsername: "Ganzs919",<
+
+  vercelToken: "vercel_xxxxxxxxx",
+  netlifyToken: "netlify_xxxxxxxxx",
+
+  // Premium system
+  owner: ["7828164131"],
+
+  // Channel & Group check
+  channel: "@thevoidslab",
+  group: "@Xprimexxvi",
+
+  // Thumbnail bot
+  thumb: "https://telegra.ph/file/xxx.jpg",
+
+  // Free premium config
+  freePremiumDays: 3 
+};
